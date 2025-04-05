9b19c79c4e6c198d2f47b537ac160618fb7a6da5
xiawpohr <xiawpohr@gmail.com>
2025-04-05 11:46:07 +0800

    feat: register get-account tool

---
+++ packages/metamask-mcp/src/tools/get-account.ts
@@ -0,0 +1,29 @@
+import { FastMCP } from "fastmcp";
+import { z } from "zod";
+import { getAccount } from "@wagmi/core"
+import { wagmiConfig } from "../wagmi-config";
+import { JSONStringify } from "../utils/json-stringify";
+
+export function registerGetAccountTools(server: FastMCP): void {
+  server.addTool({
+    name: "get-account",
+    description: "Get current account",
+    parameters: z.object({}),
+    execute: async () => {
+      const result = getAccount(wagmiConfig)
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSONStringify({
+              address: result.address,
+              addresses: result.addresses,
+              chainId: result.chainId,
+              status: result.status,
+            }),
+          },
+        ],
+      }
+    },
+  });
+};
