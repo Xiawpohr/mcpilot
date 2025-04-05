4282df51e0eef28fc0d541a0b6c23be74dd9cfb7
xiawpohr <xiawpohr@gmail.com>
2025-04-05 11:52:55 +0800

    feat: register wait-seconds tool

---
+++ packages/metamask-mcp/src/tools/wait-seconds.ts
@@ -0,0 +1,35 @@
+import { FastMCP } from "fastmcp";
+import { z } from "zod";
+import { getAccount } from "@wagmi/core"
+import { wagmiConfig } from "../wagmi-config";
+import { JSONStringify } from "../utils/json-stringify";
+
+export function registerWaitSecondsTools(server: FastMCP): void {
+  server.addTool({
+    name: "wait-seconds",
+    description: "Wait the given seconds",
+    parameters: z.object({
+      seconds: z.coerce.number()
+    }),
+    execute: async (args) => {
+      const seconds = args.seconds
+      await wait(seconds * 1000)
+      return {
+        content: [
+          {
+            type: "text",
+            text: `Wait for ${seconds} seconds`,
+          },
+        ],
+      }
+    },
+  });
+};
+
+async function wait(timeout: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(undefined)
+    }, timeout)
+  })
+}
