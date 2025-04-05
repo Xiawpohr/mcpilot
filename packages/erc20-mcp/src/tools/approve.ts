2fce5eb157c05541f08f0c764e37d61e3810671d
xiawpohr <xiawpohr@gmail.com>
2025-04-05 17:03:57 +0800

    feat: register approve tool

---
+++ packages/erc20-mcp/src/tools/approve.ts
@@ -0,0 +1,45 @@
+import { FastMCP } from "fastmcp";
+import { Address, encodeFunctionData, parseUnits } from "viem";
+import { z } from "zod";
+import erc20Abi from "../erc20-abi";
+
+export function registerApproveTools(server: FastMCP): void {
+  server.addTool({
+    name: "prepare-approve",
+    description: "Prepare for approving someone to spend ERC20 token",
+    parameters: z.object({
+      tokenAddress: z.string().startsWith("0x"),
+      tokenDecimal: z.coerce.number(),
+      spender: z.string().startsWith("0x"),
+      amount: z.coerce.string(),
+    }),
+    execute: async (args) => {
+      const tokenAddress = args.tokenAddress as Address;
+      const tokenDecimal = args.tokenDecimal;
+      const spender = args.spender as Address;
+      const amount = args.amount;
+      
+      const data = encodeFunctionData({
+        abi: erc20Abi,
+        functionName: 'approve',
+        args: [
+          spender,
+          parseUnits(amount, tokenDecimal)
+        ]
+      })
+
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSON.stringify({
+              prepareForAction: "send-transaction",
+              address: tokenAddress,
+              data,
+            })
+          }
+        ]
+      }
+    },
+  });
+};
