1d945d1b504cccb643154c1a74e05f635759e089
xiawpohr <xiawpohr@gmail.com>
2025-04-05 11:48:45 +0800

    feat: register get-ens-address tool

---
+++ packages/metamask-mcp/src/tools/get-ens-address.ts
@@ -0,0 +1,35 @@
+import { FastMCP } from "fastmcp";
+import { z } from "zod";
+import { normalize } from "viem/ens";
+import { getEnsAddress } from "@wagmi/core"
+import { wagmiConfig } from "../wagmi-config";
+
+export function registerGetENSAddressTools(server: FastMCP): void {
+  server.addTool({
+    name: "get-ens-address",
+    description: "Get the ENS address for name",
+    parameters: z.object({
+      name: z.string(),
+      chainId: z.coerce.number().optional(),
+      blockNumber: z.coerce.number().optional(),
+    }),
+    execute: async (args) => {
+      const name = args.name
+      const chainId = args.chainId as typeof wagmiConfig['chains'][number]['id']
+      const blockNumber = args.blockNumber ? BigInt(args.blockNumber) : undefined
+      const result = await getEnsAddress(wagmiConfig, {
+        name: normalize(name),
+        blockNumber,
+        chainId,
+      })
+      return {
+        content: [
+          {
+            type: "text",
+            text: result ?? "undefined",
+          },
+        ],
+      }
+    },
+  });
+};
