ff0dbab82f01842587ae303a45b40284bf2dfe3d
xiawpohr <xiawpohr@gmail.com>
2025-04-05 11:52:30 +0800

    feat: register switch-chain tool

---
+++ packages/metamask-mcp/src/tools/switch-chain.ts
@@ -0,0 +1,42 @@
+import { FastMCP } from "fastmcp";
+import { z } from "zod";
+import { switchChain } from "@wagmi/core"
+import { wagmiConfig } from "../wagmi-config";
+import { JSONStringify } from "../utils/json-stringify";
+
+export function registerSwitchChainTools(server: FastMCP): void {
+  server.addTool({
+    name: "switch-chain",
+    description: "Switch the target chain",
+    parameters: z.object({
+      chainId: z.coerce.number(),
+      addEthereumChainParameter: z.object({
+        chainName: z.string(),
+        nativeCurrency: z.object({
+          name: z.string(),
+          symbol: z.string(),
+          decimals: z.coerce.number(),
+        }).optional(),
+        rpcUrls: z.string().array(),
+        blockExplorerUrls: z.string().array().optional(),
+        iconUrls: z.string().array().optional(),
+      }).optional()
+    }),
+    execute: async (args) => {
+      const chainId = args.chainId as typeof wagmiConfig['chains'][number]['id']
+      const addEthereumChainParameter = args.addEthereumChainParameter
+      const result = await switchChain(wagmiConfig, {
+        chainId,
+        addEthereumChainParameter,
+      })
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSONStringify(result),
+          },
+        ],
+      }
+    },
+  });
+};
