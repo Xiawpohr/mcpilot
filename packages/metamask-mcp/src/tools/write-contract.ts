b427a806951a57676385112cbc1333a7dd3332f3
xiawpohr <xiawpohr@gmail.com>
2025-04-05 11:52:12 +0800

    feat: register write-contract tool

---
+++ packages/metamask-mcp/src/tools/write-contract.ts
@@ -0,0 +1,62 @@
+import { FastMCP } from "fastmcp";
+import { z } from "zod";
+import { writeContract } from "@wagmi/core"
+import { wagmiConfig } from "../wagmi-config";
+import { Abi, Address, TransactionExecutionError } from "viem";
+import { JSONStringify } from "../utils/json-stringify";
+
+export function registerWriteContractTools(server: FastMCP): void {
+  server.addTool({
+    name: "write-contract",
+    description: "Execute a write function on a contract",
+    parameters: z.object({
+      abi: z.string(),
+      address: z.string(),
+      functionName: z.string(),
+      args: z.string().array()
+    }),
+    execute: async (_args) => {
+      try {
+        const abi = JSON.parse(_args.abi) as Abi
+        const address = _args.abi as Address
+        const functionName = _args.functionName
+        const args = _args.args
+        const result = await writeContract(wagmiConfig, {
+          abi,
+          address,
+          functionName,
+          args,
+        })
+        return {
+          content: [
+            {
+              type: "text",
+              text: JSONStringify({
+                hash: result
+              }),
+            },
+          ],
+        }
+      } catch (error) {
+        if (error instanceof TransactionExecutionError) {
+          return {
+            content: [
+              {
+                type: "text",
+                text: error.cause.message,
+              }
+            ]
+          }
+        }
+        return {
+          content: [
+            {
+              type: "text",
+              text: (error as Error).message,
+            }
+          ]
+        }
+      }
+    },
+  });
+};
