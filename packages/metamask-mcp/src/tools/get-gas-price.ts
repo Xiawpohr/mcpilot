127c0cf68e59ebad0b318452b265a626c2621906
xiawpohr <xiawpohr@gmail.com>
2025-04-05 11:49:28 +0800

    feat: register get-gas-price tool

---
+++ packages/metamask-mcp/src/tools/get-gas-price.ts
@@ -0,0 +1,28 @@
+import { FastMCP } from "fastmcp";
+import { z } from "zod";
+import { getGasPrice } from "@wagmi/core"
+import { wagmiConfig } from "../wagmi-config";
+
+export function registerGetGasPriceTools(server: FastMCP): void {
+  server.addTool({
+    name: "get-gas-price",
+    description: "Get the current price of gas (in wei)",
+    parameters: z.object({
+      chainId: z.coerce.number().optional(),
+    }),
+    execute: async (args) => {
+      const chainId = args.chainId as typeof wagmiConfig['chains'][number]['id']
+      const result = await getGasPrice(wagmiConfig, {
+        chainId,
+      })
+      return {
+        content: [
+          {
+            type: "text",
+            text: result.toString(),
+          },
+        ],
+      }
+    },
+  });
+};
