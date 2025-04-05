58bfd4f4527ca75634f900b1006e77d6845d8d79
xiawpohr <xiawpohr@gmail.com>
2025-04-05 11:50:24 +0800

    feat: register get-transaction-receipt tool

---
+++ packages/metamask-mcp/src/tools/get-transaction-receipt.ts
@@ -0,0 +1,33 @@
+import { FastMCP } from "fastmcp";
+import { z } from "zod";
+import { getTransactionReceipt } from "@wagmi/core"
+import { wagmiConfig } from "../wagmi-config";
+import { type Address } from "viem";
+import { JSONStringify } from "../utils/json-stringify";
+
+export function registerGetTransactionReceiptTools(server: FastMCP): void {
+  server.addTool({
+    name: "get-transaction-receipt",
+    description: "Get the Transaction Receipt given a Transaction hash",
+    parameters: z.object({
+      hash: z.string(),
+      chainId: z.coerce.number().optional(),
+    }),
+    execute: async (args) => {
+      const hash = args.hash as Address
+      const chainId = args.chainId as typeof wagmiConfig['chains'][number]['id']
+      const result = await getTransactionReceipt(wagmiConfig, {
+        hash,
+        chainId,
+      })
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSONStringify(result),
+          },
+        ],
+      }
+    },
+  });
+};
