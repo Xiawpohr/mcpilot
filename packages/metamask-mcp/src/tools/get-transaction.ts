07b6eaf80cc92b1f7adab034bf632ad4554afa70
xiawpohr <xiawpohr@gmail.com>
2025-04-05 11:50:05 +0800

    feat: register get-transaction tool

---
+++ packages/metamask-mcp/src/tools/get-transaction.ts
@@ -0,0 +1,33 @@
+import { FastMCP } from "fastmcp";
+import { z } from "zod";
+import { getTransaction } from "@wagmi/core"
+import { wagmiConfig } from "../wagmi-config";
+import { type Address } from "viem";
+import { JSONStringify } from "../utils/json-stringify";
+
+export function registerGetTransactionTools(server: FastMCP): void {
+  server.addTool({
+    name: "get-transaction",
+    description: "Get the transactions given hashes or chain identifiers",
+    parameters: z.object({
+      hash: z.string(),
+      chainId: z.coerce.number().optional(),
+    }),
+    execute: async (args) => {
+      const hash = args.hash as Address
+      const chainId = args.chainId as typeof wagmiConfig['chains'][number]['id']
+      const result = await getTransaction(wagmiConfig, {
+        hash,
+        chainId,
+      })
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSONStringify(result),
+          },
+        ],
+      }
+    },
+  });
+};
