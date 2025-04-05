928ecf3380169fbd1ead5dde2a1d1d69f3e3d1dc
xiawpohr <xiawpohr@gmail.com>
2025-04-05 11:48:08 +0800

    feat: register call tool

---
+++ packages/metamask-mcp/src/tools/call.ts
@@ -0,0 +1,59 @@
+import { FastMCP } from "fastmcp";
+import { z } from "zod";
+import { call } from "@wagmi/core"
+import { wagmiConfig } from "../wagmi-config";
+import { Address, TransactionExecutionError } from "viem";
+import { JSONStringify } from "../utils/json-stringify";
+
+export function registerCallTools(server: FastMCP): void {
+  server.addTool({
+    name: "call",
+    description: "Executing a new message call immediately without submitting a transaction to the network",
+    parameters: z.object({
+      to: z.string(),
+      value: z.coerce.number(),
+      data: z.string(),
+    }),
+    execute: async (args) => {
+      try {
+        const to = args.to as Address
+        const value = args.value ? BigInt(args.value) : undefined
+        const data = args.data as Address
+        const result = await call(wagmiConfig, {
+          to,
+          value,
+          data,
+        })
+        return {
+          content: [
+            {
+              type: "text",
+              text: JSONStringify({
+                hash: result
+              }),
+            },
+          ],
+        }
+      } catch (error) {
+        if (error instanceof TransactionExecutionError) {
+          return {
+            content: [
+              {
+                type: "text",
+                text: error.cause.message,
+              }
+            ]
+          }
+        }
+        return {
+          content: [
+            {
+              type: "text",
+              text: (error as Error).message,
+            }
+          ]
+        }
+      }
+    },
+  });
+};
